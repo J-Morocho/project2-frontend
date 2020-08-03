47e3fb809731fb0f387c6acc7422614034fcd9f7
JDM-B550 <jendri@jdm-b550.home>
2020-08-03 16:21:19 -0400

    display data on home page

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
+
+
 const deployedURL = null
 const URL = deployedURL ? deployedURL : "http://localhost:3000"
 
 
+
+
+const getAllEvents = async () => {
+    const response = await fetch(`${URL}/events/getAll`)
+    const d = await response.json()
+    
+    d.forEach( obj => createEventCard(obj))
+}
+
+let o1 = getAllEvents()
+
 // From an obj populate the event card
 const createEventCard = async(obj) => {
-    const $h3EventName = $('<h3>').attr('class', "event_name").text("test")
-    const $pEventBorough = $('<p>').att('class', "event_borough").text("test")
-    const $pEventLocation = $('<p>').attr('class', "event_location").text("test")
-    const $pStartDateTime = $('<p>').attr('class', "start_date_time").text("test")
-    const $pEndDateTime = $('<p>').attr('class', "end_date_time").text("test")
+    const $h3EventName = $('<h3>').attr('class', "event_name").text(obj.event_name)
+    const $pEventBorough = $('<p>').attr('class', "event_borough").text(obj.event_borough)
+    const $pEventLocation = $('<p>').attr('class', "event_location").text(obj.event_location)
+    const $pStartDateTime = $('<p>').attr('class', "start_date_time").text(obj.start_date_time)
+    const $pEndDateTime = $('<p>').attr('class', "end_date_time").text(obj.$pEndDateTime)
     const $divEvent = $('<div>').attr('class', "event")
+    const $divEventCard = $('<div>').attr('class', 'event-card')
     $divEvent.append([$h3EventName, $pEventBorough, $pEventLocation, $pStartDateTime, $pEndDateTime])
     $divEventCard.append([$divEvent])
     $('#flex-container').append($divEventCard)
 }
+
+
 
 const populateDatalist = async (field, id) => {
     const data = await fetch(`${URL}/events/distinct/${field}`)
@@ -26,11 +42,6 @@ const populateDatalist = async (field, id) => {
 }
 
 
-const getAllEvents = async () => {
-    const response = await fetch(`${URL}/events/getAll`)
-    const data = await response.json()
-    console.log(data)
-}
 
 populateDatalist("event_borough", "locations")
 populateDatalist("event_type", "eventTypes")
