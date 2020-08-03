4607720318d9e4f76b77de24b983a969cba2d647
JDM-B550 <jendri@jdm-b550.home>
2020-08-03 12:13:28 -0400

    add createEventCard

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 
-const data = fetch("https://data.cityofnewyork.us/resource/tvpp-9vvx.json").then(res => res.json())
-console.log(data)
+// From an obj populate the event card
+function createEventCard(obj) {
+    const 
+    const $pEventBorough = $('<p>').att("class", "event_borough")
+    const $pEventLocation = $('<p>').attr("class", "event_location")
+    const $pStartDateTime = $('<p>').attr('class', "start_date_time")
+    const $pEndDateTime = $('<p>').attr('class', "end_date_time")
+    const card = $('<div>').attr('class', "event-card")
+
+}
