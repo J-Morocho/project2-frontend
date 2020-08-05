4af24a55f2d3e5fa85035b7a896b95cc74de2337
JDM-B550 <jendri@jdm-b550.home>
2020-08-05 10:03:14 -0400

    link register button and implement delete user

--- register.js
+++ register.js
@@ -1,3 +1,26 @@
+const { create } = require("../project2-backend/models/user")
+
 const deployedURL = "https://project2-backend-hosted.herokuapp.com"
 const URL = deployedURL ? deployedURL : "http://localhost:3000"
 
+
+$('#register-button').on('click', async () => {
+    // Check if name input field is not blank
+    if ( $('#registerNameInput').val() !== "") {
+        const name = $('#registerNameInput').val()
+        
+        const newUser = {
+            name: name,
+            eventsAttending: []
+        }
+
+        const response = await fetch(`${URL}/users/create`, {
+            method: "post",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newUser)
+        } )
+        console.log(`registration successfull!`)
+    } else {
+        console.error('NAME INPUT ERROR')
+    }
+})
