48a6faacb357257382ecd771104bcd511f3d344b
JDM-B550 <jendri@jdm-b550.home>
2020-08-05 14:49:21 -0400

    add card styling

--- app.js
+++ app.js
@@ -30,14 +30,14 @@ $('#queryuser').on('click', getUserEvents)
 
 // From an obj populate the event card
 const createEventCard = async(obj) => {
-    const $h3EventName = $('<h3>').attr('class', "event_name").text(obj.event_name)
+    const $h3EventName = $('<h3>').attr('class', "event_name card-title").text(obj.event_name)
     const $pEventBorough = $('<p>').attr('class', "event_borough").text(obj.event_borough)
     const $pEventLocation = $('<p>').attr('class', "event_location").text(obj.event_location)
     const $pStartDateTime = $('<p>').attr('class', "start_date_time").text(obj.start_date_time)
     const $pEndDateTime = $('<p>').attr('class', "end_date_time").text(obj.$pEndDateTime)
-    const $divEvent = $('<div>').attr('class', "event")
+    const $divEvent = $('<div>').attr('class', "event card-body")
     const $divEventCard = $('<div>').attr({
-        class: 'event-card',
+        class: 'event-card card',
         id: obj._id        
     })
     const $addEventButton = $('<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">').attr('id', obj._id).text('Add Event')
